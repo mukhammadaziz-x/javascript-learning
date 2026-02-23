0db0e7960581b01de5bd6a60525843c8d3ab9b11
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 21:18:37 +0500

    camelCase style

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,10 +1,25 @@
 // alert('Hello World!');
 // alert('Mening ismim Muhammadaziz');
 
-let name = "Muhammadaziz";
-let surname = "Xabibullayev";
-let age = 20
+// let name = "Muhammadaziz";
+// let surname = "Xabibullayev";
+// let age = 20
 
-console.log(name);
-console.log(surname);
-console.log(age);
+// console.log(name);
+// console.log(surname);
+// console.log(age);
+
+// console.log(name);
+// let name = "Muhammadaziz";
+
+// O'zgaruvchilarga nom berish
+// O'zgaruvchilarga nom berayotganda, u o'zida qanday ma'lumot saqlashini hisobga
+// olib, berilgan nom saqlayotgan ma'lumotni ifoda etib turishi kerak. O'zgaruvchilar
+// odatda camelCase ko'rinishida yoziladi.
+let name = "Muhammadaziz"; // understandable
+let var1 = "Muhammadaziz"; // misunderstandable
+
+let userName = "username"; // correct (camelCase style)
+let user_name = "username"; // incorrect
+
+ 
