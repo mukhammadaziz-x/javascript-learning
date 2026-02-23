33c79f8caac28e5f39155551fb611858845d65dd
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 22:07:49 +0500

    Data types, primitive and object

--- lesson2/app.js
+++ lesson2/app.js
@@ -8,7 +8,43 @@ let price = 19.99;
 // console.log(price)
 
 // String - o'zida belgilar ketma-ketligini saqlovchi ma'lumot turi
-let firstName = "Muhammadaziz"
+let firstName = "Muhammadaziz";
 // console.log(firstName)
 
+// Boolean - mantiqiy ma'lumot turi bo'lib faqatgina true (rost) yoki false (yolg'on)
+// qiymatlarni qabul qilishi mumkin bo'lgan ma'lumot turi.
+let isUser = true
+// console.log(isUser)
 
+// Undefined - hali aniqlanmagan ma'lumot turi.
+let student;
+// console.log(student);
+
+// Null - bo'sh qiymatga ega bo'lgan ma'lumot turi.
+let school = null;
+// console.log(school);
+
+// Symbol - yagon (unique) va o'zgarmas qiymatga ega bo'luvchi ma'lumot turi.
+let id = Symbol('id');
+// console.log(id);
+
+//Bigint - katta sonlarni saqlashga mo'ljallangan ma'lumot turi.
+const alsoHuge = BigInt(325234234242423424);
+// console.log(alsoHuge);
+
+// JavaScript dynamic typing funksiyasiga ega bo'lib uning yordamida o'zgaruvchi
+// saqlovchi ma'lumot turini ko'rsatmasdan ham qiymat saqlashimiz mumkin.
+// O'zgaruvchi saqlaydigan ma'lumot turi avtomatik tarzda aniqlanadi.
+
+// type of - operatoridan foydalanib o'zgaruvchi saqlayotgan ma'lumotning turini
+// aniqlash imkoniyati mavjud.
+
+let myAge = 24;
+// console.log(typeof myAge)
+
+// Object turdagi qiymatlar
+// Object qiymat o'zgaruvchi murojaat qilgan manzilda ko'rsatilgan boshqa manzilda saqlanadi.
+
+// Function
+// Array
+// Object
