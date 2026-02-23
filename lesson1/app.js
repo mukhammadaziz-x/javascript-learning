effa78090537971218c71663baeaeacb62181106
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 21:13:29 +0500

    let variable

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,2 +1,10 @@
-alert('Hello World!');
-alert('Mening ismim Muhammadaziz');
+// alert('Hello World!');
+// alert('Mening ismim Muhammadaziz');
+
+let name = "Muhammadaziz";
+let surname = "Xabibullayev";
+let age = 20
+
+console.log(name);
+console.log(surname);
+console.log(age);
