e15d9f501f634611c3fdd58b23d7a5808a91a976
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 21:21:41 +0500

    incorrect type codes

--- lesson1/app.js
+++ lesson1/app.js
@@ -16,10 +16,15 @@
 // O'zgaruvchilarga nom berayotganda, u o'zida qanday ma'lumot saqlashini hisobga
 // olib, berilgan nom saqlayotgan ma'lumotni ifoda etib turishi kerak. O'zgaruvchilar
 // odatda camelCase ko'rinishida yoziladi.
-let name = "Muhammadaziz"; // understandable
-let var1 = "Muhammadaziz"; // misunderstandable
+// let name = "Muhammadaziz"; // understandable
+// let var1 = "Muhammadaziz"; // misunderstandable
 
-let userName = "username"; // correct (camelCase style)
-let user_name = "username"; // incorrect
+// let userName = "username"; // correct (camelCase style)
+// let user_name = "username"; // incorrect
 
- 
+let with space = "withSpace";
+let 1StartsWithNumber = "startsWithNumber";
+let other@SymbolUsed = "otherSymbolUsed";
+let for = "Reserved word";
+let firstName = "Muhammadaziz";
+let firsTName = "Muhammadaziz";
