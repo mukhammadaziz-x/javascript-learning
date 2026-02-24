97b887b28306d4d07e26fe8377b163f5eb0218d5
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-24 21:59:42 +0500

    let, const, var

---
+++ lesson3/app.js
@@ -0,0 +1,20 @@
+// 3 - Lesson: let, const, var
+'use strict';
+
+// let age = 19
+// age = 20
+// console.log(age)
+
+// const id = 'AA0';
+// console.log(id);
+// id = 'AA1'; // X
+// console.log(id) // X
+
+// var name = 'Muhammadaziz';
+// console.log(name)
+// name = 'Muhammadali';
+// console.log(name)
+
+// use strict mode
+// firstName = 'Muhammadaziz'
+// console.log(firstName)
