130f076e651a8bcadbf4451fb3aad3a60c458427
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-24 22:24:38 +0500

    arifmetik operatorlar

---
+++ lesson4/app.js
@@ -0,0 +1,17 @@
+// Lesson 4: arifmetik operatorlar, operatorlar
+
+// let a = 5, b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(b--);
+// console.log(b);
+// console.log(--b);
+// console.log(b);
+
