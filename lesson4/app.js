eb98b873c3e0a3034395d4e4182a862eafd36c31
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-24 22:30:41 +0500

    tayinlash operatorlari

--- lesson4/app.js
+++ lesson4/app.js
@@ -30,3 +30,19 @@
 // console.log(a >= b); // false
 // console.log(a <= b); // true
 
+
+let a = 5, b = 10, c = 15;
+
+console.log(a);
+a = b; // 10
+console.log(a);
+a += b; // 20
+console.log(a);
+a -= b; // 10
+console.log(a);
+c *= b; // 150
+console.log(c);
+c /= 5; // 30
+console.log(c);
+b %= 2; // 0
+console.log(b);
