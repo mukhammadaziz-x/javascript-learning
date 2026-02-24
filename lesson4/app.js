5d9d59b98d960bd27b374c6c25f2fd2f2403f272
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-24 22:25:05 +0500

    operatorlar

--- lesson4/app.js
+++ lesson4/app.js
@@ -15,3 +15,18 @@
 // console.log(--b);
 // console.log(b);
 
+
+// let a = 5, b = 10, c = '5';
+// const z = a
+
+// console.log(a == c); // true
+// console.log(a === c); // false
+// console.log(a == z); // true
+// console.log(a === z); // true
+// console.log(a != b); // true
+// console.log(a != z); // false
+// console.log(a > b); // false
+// console.log(a < b); // true
+// console.log(a >= b); // false
+// console.log(a <= b); // true
+
