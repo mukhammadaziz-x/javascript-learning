66a2b6a25360a986e54045847c7a41b0abe16a51
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-24 22:37:20 +0500

    my first code

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,30 +1,30 @@
-// alert('Hello World!');
-// alert('Mening ismim Muhammadaziz');
+alert('Hello World!');
+alert('Mening ismim Muhammadaziz');
 
-// let name = "Muhammadaziz";
-// let surname = "Xabibullayev";
-// let age = 20
+let name = "Muhammadaziz";
+let surname = "Xabibullayev";
+let age = 20
 
-// console.log(name);
-// console.log(surname);
-// console.log(age);
+console.log(name);
+console.log(surname);
+console.log(age);
 
-// console.log(name);
-// let name = "Muhammadaziz";
+console.log(name);
+let name = "Muhammadaziz";
 
 // O'zgaruvchilarga nom berish
 // O'zgaruvchilarga nom berayotganda, u o'zida qanday ma'lumot saqlashini hisobga
 // olib, berilgan nom saqlayotgan ma'lumotni ifoda etib turishi kerak. O'zgaruvchilar
 // odatda camelCase ko'rinishida yoziladi.
-// let name = "Muhammadaziz"; // understandable
-// let var1 = "Muhammadaziz"; // misunderstandable
+let name = "Muhammadaziz"; // understandable
+let var1 = "Muhammadaziz"; // misunderstandable
 
-// let userName = "username"; // correct (camelCase style)
-// let user_name = "username"; // incorrect
+let userName = "username"; // correct (camelCase style)
+let user_name = "username"; // incorrect
 
-// let with space = "withSpace";
-// let 1StartsWithNumber = "startsWithNumber";
-// let other@SymbolUsed = "otherSymbolUsed";
-// let for = "Reserved word";
-// let firstName = "Muhammadaziz";
-// let firsTName = "Muhammadaziz";
+let with space = "withSpace"; // error
+let 1StartsWithNumber = "startsWithNumber"; // error
+let other@SymbolUsed = "otherSymbolUsed"; // error
+let for = "Reserved word"; // error
+let firstName = "Muhammadaziz";
+let firsTName = "Muhammadaziz";
