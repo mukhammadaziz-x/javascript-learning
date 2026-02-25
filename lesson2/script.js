b3d35846107829a7d6a18fff3cbdc3db302448ce
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-25 22:44:24 +0500

    string and how to write correct code

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,3 +1,15 @@
 // LESSON 2
 // THEME-1: STRING, CONCATENATION, TEMPLATE LITERALS
 
+// String - belgilar ketma-ketligidan tashkil topgan ma'lumot turi.
+// bittalik ('') yoki ikkitalik ("") qo'shtirnoqlardan foydalanib yaratish
+// imkoniyati mavjud. Ba'zi bir maxsus belgilardan tashkil topgan matnlarda
+// odatda ikkitalik qo'shtirnoqlardan foydalaniladi.
+
+// let name = 'Otabek';
+// // let firstName = 'Og'abek'
+// console.log(name);
+// let surname = "Haydarov";
+// // let lastName = "O"tkir"
+// console.log(surname);
+
