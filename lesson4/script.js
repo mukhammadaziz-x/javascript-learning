586a890b12a01e69d49ae7f5f34b375f6faa7b92
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-04 07:42:46 +0500

    learning lesson

---
+++ lesson4/script.js
@@ -0,0 +1,10 @@
+// LESSON 4
+// Mavzu-1: Array, uni yaratish, elementlarni olish, qo'shish 
+// o'zgartirish, o'chirish, array uzunligini aniqlash.
+// Mavzu-2: 
+
+
+const languages = ['js', 'java', 'go'];
+console.log(languages);
+
+const language = new Array('js', 'java', 'go');
