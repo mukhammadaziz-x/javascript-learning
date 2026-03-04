c1257ab546da6208dbe833b04ff4def2e72be868
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-04 08:24:46 +0500

    updated

--- lesson4/script.js
+++ lesson4/script.js
@@ -4,7 +4,44 @@
 // Mavzu-2: 
 
 
-const languages = ['js', 'java', 'go'];
-console.log(languages);
+// const languages = ['js', 'java', 'go'];
+// console.log(languages);
 
-const language = new Array('js', 'java', 'go');
+// const language = new Array('js', 'java', 'go');
+// console.log(language)
+
+// Array o'z ichida turli xil qiymatlarni saqlashi mumkin.
+// bo'sh array
+// const boshArray = [];
+
+// sonlar array
+// const sonArray = [1,3,8,4]
+
+// string array
+// const stringArray = ['Javascript', 'Java', 'Python']
+
+// aralash array
+// const aralash = ['Javascrip', 2, false, null]
+
+// Array elementini olish
+// 
+// const helloArray = ['h', 'e', 'l', 'l', 'o']
+
+// console.log(helloArray[0])
+// console.log(helloArray[4])
+
+// Array element qo'shish
+// push() metodi yordamida array oxiriga element qo'shish mumkin.
+// let languages = ['js', 'python', 'go'];
+
+// languages.push('ruby');
+
+// console.log(languages)
+
+// unshift() metodi yordamida array boshiga element qo'shish mumkin.
+// const languages = ['c', 'c++', 'js']
+
+// languages.unshift('python')
+
+// console.log(languages)
+
