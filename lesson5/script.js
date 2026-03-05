c4ffdff891b3cd50338c7dbdabff252fa5d0c744
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-05 08:22:42 +0500

    Lesson 5

---
+++ lesson5/script.js
@@ -0,0 +1,14 @@
+// LESSON 5
+// Mavzu-1: ARRAY metodlari
+
+// concat()
+const array1 = [1, 4];
+const array2 = [2, 3, 7];
+const result = array1.concat(array2);
+
+console.log(result);
+
+// find()
+const findValue = (number) => number === 2;
+const foundValue = result.find(findValue);
+console.log(foundValue);
