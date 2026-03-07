2444bd72627162a401cc61a0ab84650d9e1e8c12
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-07 11:30:01 +0500

    updated

--- lesson5/script.js
+++ lesson5/script.js
@@ -2,11 +2,7 @@
 // // Mavzu-1: ARRAY metodlari
 
 // // concat()
-// const array1 = [1, 4];
-// const array2 = [2, 3, 7];
-// const result = array1.concat(array2);
 
-// console.log(result);
 
 // // find()
 
