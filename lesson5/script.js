b4a51c723552db9981638ab55b5a4d452b67c8aa
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-07 10:42:43 +0500

    updated

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,29 +1,90 @@
-// LESSON 5
-// Mavzu-1: ARRAY metodlari
+// // LESSON 5
+// // Mavzu-1: ARRAY metodlari
 
-// concat()
-const array1 = [1, 4];
-const array2 = [2, 3, 7];
-const result = array1.concat(array2);
+// // concat()
+// const array1 = [1, 4];
+// const array2 = [2, 3, 7];
+// const result = array1.concat(array2);
 
-console.log(result);
+// console.log(result);
 
-// find()
-const findValue = (number) => number === 2;
-const foundValue = result.find(findValue);
-console.log(foundValue);
+// // find()
+// const findValue = (number) => number === 2;
+// const foundValue = result.find(findValue);
+// console.log(foundValue);
 
-// filter()
-let filterValues = number => number > 5;
-filterValues = result.filter(filterValues)
+// // filter()
+// let filterValues = number => number > 5;
+// filterValues = result.filter(filterValues)
 
-console.log(filterValues)
+// console.log(filterValues)
 
-// map()
-const mapValues = number => 'Son: ' + number;
-const mappedValues = result.map(mapValues);
+// // map()
+// const mapValues = number => 'Son: ' + number;
+// const mappedValues = result.map(mapValues);
 
-console.log(mappedValues);
+// console.log(mappedValues);
 
-// sort()
- 
+// // sort()
+// result.sort()
+// console.log(result)
+
+
+// Mavzu-2: Object
+// const developer = {
+//     name: 'Muhammadaziz',
+//     age: 19
+// }
+
+// console.log(developer)
+
+
+// Object e'lon qilish (yaratish)
+// const developer = {
+//     name: 'Muhammadaziz',
+//     age: 19
+// }
+
+// console.log(typeof developer)
+
+// Object xossasini olish
+// Xossaning qiymatini kalit yordamida olish imkoniyati mavjud.
+
+// Dot (.) belgisidan foydalanib
+// const developer = {
+//     name: 'Ahmad',
+//     age: 96,
+// }
+
+// console.log(developer.name); // Ahmad
+
+// Qavs [] belgisidan foydalanib
+// const developer = {
+//     name: 'Toshmat',
+//     age: 37
+// }
+
+// console.log(developer['name'])
+
+// Nested (ichma-ich) objectlar
+// const developer = {
+//     name: 'Jamshid',
+//     age: 20,
+//     address: {
+//         country: 'Uzbekistan',
+//         city: 'Tashkent',
+//         house: 49
+//     }
+// }
+// console.log(developer)
+
+// Object method'lari
+// const person = {
+//     name: 'Sarvinoz',
+//     age: 32,
+//     greeting: function() {         // method
+//         console.log('Hello!');
+//     }
+// }
+
+// person.greeting()
